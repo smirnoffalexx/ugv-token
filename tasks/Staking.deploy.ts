8a37ee2ecdfdad3c47ff4bfaa0fa9183628faa72
alexxander18 <hockeyman98@mail.ru>
2023-01-03 21:23:14 +0300

    update packages and fix deploy script

--- tasks/Staking.deploy.ts
+++ tasks/Staking.deploy.ts
@@ -1,12 +1,13 @@
 import { task } from "hardhat/config";
 
 task("deploy:staking", "Deploy Staking contract").setAction(
-    async function ({ _ }, { getNamedAccounts, deployments: { deploy } }) {
+    async function ({ _ }, { getNamedAccounts, deployments: { deploy } , ethers: { getContract } }) {
         const { deployer } = await getNamedAccounts();
+        const ugvToken = await getContract("UGVToken");
 
         return await deploy("UGVStaking", {
             from: deployer,
-            args: [],
+            args: [ugvToken.address],
             log: true,
         });
     }
