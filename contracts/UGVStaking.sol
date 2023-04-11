20ba51c4c378f03cde13b749aaa8586e665ddc42
alexxander18 <hockeyman98@mail.ru>
2023-04-12 00:52:17 +0300

    small fixes

--- contracts/UGVStaking.sol
+++ contracts/UGVStaking.sol
@@ -1,6 +1,6 @@
 // SPDX-License-Identifier: MIT
 
-pragma solidity ^0.8.4;
+pragma solidity 0.8.17;
 
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 
@@ -14,11 +14,6 @@ contract UGVStaking {
         Free,
         Partial,
         Maxima
-    }
-
-    struct LockedBalance {
-        uint256 amount;
-        uint256 duration;
     }
 
     struct StakeData {
